1a5208e400f48fb6d59c863e8eb0153976ab77af
msdn <msdn@MONKI>
2019-11-15 14:21:38 +0900

    MyFriendDTO Change - John(11/15)

--- TeaTime/src/main/java/com/care/modelDTO/MyFriendDTO.java
+++ TeaTime/src/main/java/com/care/modelDTO/MyFriendDTO.java
@@ -4,6 +4,7 @@ public class MyFriendDTO {
 
 	private String m_id;
 	private String f_id;
+	private int f_isfriend;
 	
 	public String getM_id() {
 		return m_id;
@@ -17,6 +18,13 @@ public class MyFriendDTO {
 	public void setF_id(String f_id) {
 		this.f_id = f_id;
 	}
+	public int getF_isfriend() {
+		return f_isfriend;
+	}
+	public void setF_isfriend(int f_isfriend) {
+		this.f_isfriend = f_isfriend;
+	}
+	
 	
 	
 	
